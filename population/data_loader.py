c9dc9dd70532806b026ab565920a430ab856b779
Gunnar Arctaedius <gunnar@arctaedius.com>
2025-11-01 13:20:24 +0100

    Added ability to load data from cache

--- population/data_loader.py
+++ population/data_loader.py
@@ -1,8 +1,23 @@
 import rasterio
+from numpy import ndarray
+from affine import Affine
 
-def load_population_data():
-    with rasterio.open("data/global_pop_2025_CN_1km_R2025A_UA_v1.tif") as src:
-        data = src.read(1)  # read population count array (on level 1)
-        transform = src.transform  # maps pixel coords -> lat/lon
+import pickle
+
+def load_population_data() -> tuple[ndarray, Affine]:
+    cache_file_location = "population\\cache.pkl"
+    
+    # Try loading from cache if available
+    try:
+        with open(cache_file_location, "rb") as cache_file:
+            data, transform = pickle.load(cache_file)
+
+    except:
+        with rasterio.open("data/global_pop_2025_CN_1km_R2025A_UA_v1.tif") as src:
+            data = src.read(1)  # read population count array (on level 1)
+            transform = src.transform  # maps pixel coords -> lat/lon
+
+        with open(cache_file_location, "wb") as cache_file:
+            pickle.dump((data, transform), cache_file)
 
     return data, transform
