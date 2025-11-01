8d37a155476b3e518ed93c55517c11579f674497
Gunnar Arctaedius <gunnar@arctaedius.com>
2025-11-01 13:05:02 +0100

    Added methods for reading data and selecting a random person and finding their pixel

--- population/data_loader.py
+++ population/data_loader.py
@@ -1,4 +1,8 @@
 import rasterio
 
 def load_population_data():
-    return (0,0)
+    with rasterio.open("data/global_pop_2025_CN_1km_R2025A_UA_v1.tif") as src:
+        data = src.read(1)  # read population count array (on level 1)
+        transform = src.transform  # maps pixel coords -> lat/lon
+
+    return data, transform
--- population/sampler.py
+++ population/sampler.py
@@ -1,5 +1,27 @@
 import numpy as np
-import random
+from rasterio.transform import xy
+from affine import Affine
 
-def sample_weighted_location(data, transform):
-    pass
+def sample_weighted_location(data: np.ndarray, transform: Affine) -> tuple[float, float]:
+    # Mask out negative cells (where no one lives)
+    mask = data > 0
+
+    # Get pixel indices of populated pixels
+    rows, cols = np.where(mask)
+
+    # Get an array with all valid population counts
+    pop = data[mask]
+
+    # Compute sampling probabilities proportional to population
+    probabilities = pop / pop.sum()
+
+    # Select a random pixel (wieghted by population)
+    pixel_index = np.random.choice(len(pop), p=probabilities)
+
+    row, col = rows[pixel_index], cols[pixel_index]
+
+    lon, lat = xy(transform, row, col)
+
+    return (lat, lon)
+
+    
